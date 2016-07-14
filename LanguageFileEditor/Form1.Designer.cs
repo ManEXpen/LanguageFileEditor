7fc7d7894500f2d62ddf7ad4c322604b7e022aec
ManEXpen <manexpen@gmail.com>
2016-07-15 03:24:02 +0900

    FirstCommit

---
+++ LanguageFileEditor/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace LanguageFileEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new MetroFramework.Controls.MetroGrid();
+            this.DelBtn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.selectLangType = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.initContents = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.edited = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new MetroFramework.Controls.MetroButton();
+            this.button2 = new MetroFramework.Controls.MetroButton();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DelBtn,
+            this.selectLangType,
+            this.initContents,
+            this.edited});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGridView1.Location = new System.Drawing.Point(-3, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(497, 497);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // DelBtn
+            // 
+            this.DelBtn.HeaderText = "削除";
+            this.DelBtn.Name = "DelBtn";
+            // 
+            // selectLangType
+            // 
+            this.selectLangType.HeaderText = "タイプの選択";
+            this.selectLangType.Name = "selectLangType";
+            // 
+            // initContents
+            // 
+            this.initContents.HeaderText = "翻訳前";
+            this.initContents.Name = "initContents";
+            // 
+            // edited
+            // 
+            this.edited.HeaderText = "編集後";
+            this.edited.Name = "edited";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(345, 569);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "保存";
+            this.button1.UseSelectable = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(218, 569);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "開く";
+            this.button2.UseSelectable = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "ja_JP",
+            "en_US"});
+            this.metroComboBox1.Location = new System.Drawing.Point(91, 577);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(121, 29);
+            this.metroComboBox1.TabIndex = 3;
+            this.metroComboBox1.UseSelectable = true;
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(84, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(106, 537);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.Size = new System.Drawing.Size(106, 23);
+            this.metroTextBox1.TabIndex = 4;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.metroButton1.Location = new System.Drawing.Point(218, 537);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(86, 23);
+            this.metroButton1.TabIndex = 6;
+            this.metroButton1.Text = "タイプの追加";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 609);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid dataGridView1;
+        private System.Windows.Forms.DataGridViewButtonColumn DelBtn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn selectLangType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn initContents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn edited;
+        private MetroFramework.Controls.MetroButton button1;
+        private MetroFramework.Controls.MetroButton button2;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
+
---
+++ LanguageFileEditor/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Text.RegularExpressions;
+
+namespace LanguageFileEditor {
+    public partial class Form1 : MetroForm {
+        
+        public Form1() {
+            InitializeComponent();
+
+            this.selectLangType.Items.Add("item");
+            this.selectLangType.Items.Add("chat");
+            this.selectLangType.Items.Add("tile");
+            this.selectLangType.Items.Add("gui");
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+            if (e.ColumnIndex == this.dataGridView1.Columns["DelBtn"].Index) {
+                try { this.dataGridView1.Rows.RemoveAt(e.RowIndex); } catch { }
+            }
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e) {
+            if (this.metroTextBox1.Text != "") this.selectLangType.Items.Add(metroTextBox1.Text);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            StringBuilder sb = new StringBuilder();
+            SaveFileDialog sfd = new SaveFileDialog();
+            try {
+                if (metroComboBox1.SelectedItem == null) {
+                    MessageBox.Show("言語を選択してください");
+                    return;
+                }
+
+                sfd.FileName = metroComboBox1.SelectedItem.ToString();
+                sfd.Filter = "言語ファイル|*.lang";
+
+                if (sfd.ShowDialog() == DialogResult.OK) { 
+                    using (var sw = new StreamWriter(sfd.FileName, false, System.Text.Encoding.GetEncoding("UTF-8"))) {
+
+                        for (int i = 0; i < dataGridView1.RowCount - 1; i++) {
+                            var tmp = dataGridView1.Rows[i];
+                            sb.Append(tmp.Cells[1].Value.ToString()).Append(".");
+                            sb.Append(tmp.Cells[2].Value.ToString());
+                            if (tmp.Cells[1].Value.ToString() == "tile" || tmp.Cells[1].Value.ToString() == "item") sb.Append(".name");
+                            sb.Append("=");
+                            sb.Append(tmp.Cells[3].Value.ToString());
+                            sb.Append("\n");
+                        }
+                        sw.Write(sb.ToString().TrimEnd('\r', '\n'));
+                    }
+                MessageBox.Show("保存しました");
+            }
+            } catch (SystemException ex) {
+                System.Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "言語ファイル|*.lang";
+
+            if(ofd.ShowDialog() == DialogResult.OK) {
+                using(var sr = new StreamReader(new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read), Encoding.GetEncoding("UTF-8"))) {
+                    var str = sr.ReadToEnd();
+                    str = Regex.Replace(str, "^\n", "");
+                    str = str.TrimEnd('\r', '\n');
+
+                    String[] lines = str.Split('\n');
+
+                    this.dataGridView1.Rows.Clear();
+                    foreach(var tmp in lines) {
+                        if (tmp == "\n") continue;
+                        String[] list = tmp.Split('.');
+                        switch (list[0]) {
+                            case "item":
+                                this.dataGridView1.Rows.Add(new String[] {"", "item", GetUnTranslatedText(tmp, "item"), GetTranslatedText(tmp)});
+                                break;
+                            case "tile":
+                                this.dataGridView1.Rows.Add(new String[] { "", "tile", GetUnTranslatedText(tmp, "tile"), GetTranslatedText(tmp) });
+                                break;
+                            case "chat":
+                                this.dataGridView1.Rows.Add(new String[] { "", "chat", GetUnTranslatedText(tmp, "chat"), GetTranslatedText(tmp) });
+                                break;
+                            case "gui":
+                                this.dataGridView1.Rows.Add(new String[] { "", "gui", GetUnTranslatedText(tmp, "gui"), GetTranslatedText(tmp) });
+                                break;
+                            default:
+                                int idx = this.selectLangType.Items.IndexOf(list[0]);
+                                if (idx == -1) this.selectLangType.Items.Add(list[0]);
+                                this.dataGridView1.Rows.Add(new String[] { "", list[0], GetUnTranslatedText(tmp, list[0]), GetTranslatedText(tmp) });
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static string GetUnTranslatedText (string str, string type){
+            var a = str.Replace(type + ".", "");
+            Console.WriteLine(str);
+            a = a.Substring(0, a.IndexOf('='));
+            if (type == "tile" || type == "item") a = a.Replace(".name", "");
+            return a;
+        }
+        private static string GetTranslatedText(string str) {
+            var a = str.Substring(str.IndexOf('=')+1).Replace('=', '\0');
+            return a;
+        }
+
+    }
+}
